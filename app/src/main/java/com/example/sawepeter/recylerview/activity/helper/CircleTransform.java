bd2cfaa79c1b7966f3524e71852488de2206a02b
user.email <sawepeter6@gmail.com>
2018-03-20 19:05:14 +0300

    bitmap

--- app/src/main/java/com/example/sawepeter/recylerview/activity/helper/CircleTransform.java
+++ app/src/main/java/com/example/sawepeter/recylerview/activity/helper/CircleTransform.java
@@ -25,6 +25,13 @@ public class CircleTransform extends BitmapTransformation{
 
         int size = Math.min(source.getWidth(), source.getHeight());
         int x = (source.getWidth() - size) / 2;
+        int y = (source.getHeight() -size) /2;
+
+        //TODO this could be acquired from the pool too
+        Bitmap squared = Bitmap.createBitmap(source, x, y, size, size);
+
+        Bitmap result = pool.get(size, size, Bitmap.Config.ARGB_8888);
+        if (result == null)
     }
 
     @Override
