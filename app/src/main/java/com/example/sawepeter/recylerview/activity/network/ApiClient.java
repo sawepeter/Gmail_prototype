f88c9d44bdccc5420a2bc735394121025a0a0baf
user.email <sawepeter6@gmail.com>
2018-03-20 18:30:21 +0300

    network api

---
+++ app/src/main/java/com/example/sawepeter/recylerview/activity/network/ApiClient.java
@@ -0,0 +1,9 @@
+package com.example.sawepeter.recylerview.activity.network;
+
+/**
+ * Created by SAWE PETER on 3/20/2018.
+ */
+
+public class ApiClient {
+    public static final String BASE_URL  = "https://api.androidhive.info/json/";
+}
