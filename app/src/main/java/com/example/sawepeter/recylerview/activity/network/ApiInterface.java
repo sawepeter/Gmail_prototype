b2f9607b79a911a63a1d3811765939c61b1c01de
user.email <sawepeter6@gmail.com>
2018-03-20 18:44:44 +0300

    apiinterface

--- app/src/main/java/com/example/sawepeter/recylerview/activity/network/ApiInterface.java
+++ app/src/main/java/com/example/sawepeter/recylerview/activity/network/ApiInterface.java
@@ -1,8 +1,17 @@
 package com.example.sawepeter.recylerview.activity.network;
 
+import com.example.sawepeter.recylerview.activity.model.Message;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 /**
  * Created by SAWE PETER on 3/20/2018.
  */
 
-public class ApiInterface {
+public interface ApiInterface {
+    @GET("inbox.json")
+    Call<List<Message>> ger
 }
