66f735811c58d0aa638db6b022b432a912d84d4b
user.email <sawepeter6@gmail.com>
2018-03-20 18:52:20 +0300

    bitmap addition

--- app/src/main/java/com/example/sawepeter/recylerview/activity/helper/CircleTransform.java
+++ app/src/main/java/com/example/sawepeter/recylerview/activity/helper/CircleTransform.java
@@ -1,8 +1,22 @@
 package com.example.sawepeter.recylerview.activity.helper;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+
+import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
+import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
+
 /**
  * Created by SAWE PETER on 3/20/2018.
  */
 
-public class CircleTransform {
+public class CircleTransform extends BitmapTransformation{
+    public CircleTransform(Context context ) {
+        super(context);
+    }
+
+    @Override
+    protected Bitmap transform(BitmapPool pool,  Bitmap toTransform, int outWidth, int outHeight) {
+        return circleCrop(pool, toTransform);
+    }
 }
