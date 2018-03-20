5a44ec18790674bd0df8a722ffebe855bb4fc80d
user.email <sawepeter6@gmail.com>
2018-03-20 17:02:53 +0300

    activity packages

--- app/src/main/java/com/example/sawepeter/recylerview/activity/activity/home.java
+++ app/src/main/java/com/example/sawepeter/recylerview/activity/activity/home.java
@@ -1,4 +1,4 @@
-package com.example.sawepeter.recylerview.activity;
+package com.example.sawepeter.recylerview.activity.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
