c5dd9fb73ca5eabce4bd07bc51c3a43e13eb343c
user.email <sawepeter6@gmail.com>
2018-03-20 17:08:22 +0300

    messages.java changes

--- app/src/main/java/com/example/sawepeter/recylerview/activity/model/Message.java
+++ app/src/main/java/com/example/sawepeter/recylerview/activity/model/Message.java
@@ -5,4 +5,23 @@ package com.example.sawepeter.recylerview.activity.model;
  */
 
 public class Message {
+    private int id;
+    private String from;
+    private String subject;
+    private String message;
+    private String timestamp;
+    private String picture;
+    private boolean isImportant;
+    private boolean isRead;
+    private int color = -1;
+
+    public Message() {
+
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
 }
