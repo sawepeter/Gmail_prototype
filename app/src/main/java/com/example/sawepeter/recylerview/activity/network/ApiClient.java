a5e3aaf3dd67a791178edff05de2f03189a71d40
user.email <sawepeter6@gmail.com>
2018-03-20 18:36:38 +0300

    apiclient class

--- app/src/main/java/com/example/sawepeter/recylerview/activity/network/ApiClient.java
+++ app/src/main/java/com/example/sawepeter/recylerview/activity/network/ApiClient.java
@@ -1,9 +1,23 @@
 package com.example.sawepeter.recylerview.activity.network;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by SAWE PETER on 3/20/2018.
  */
 
 public class ApiClient {
     public static final String BASE_URL  = "https://api.androidhive.info/json/";
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 }
