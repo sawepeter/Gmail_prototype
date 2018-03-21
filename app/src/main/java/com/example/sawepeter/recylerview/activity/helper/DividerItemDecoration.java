208e6bc91cf335ccdef2a88a1f2996556eef8cf8
user.email <sawepeter6@gmail.com>
2018-03-21 13:10:51 +0300

    helper classes changes

--- app/src/main/java/com/example/sawepeter/recylerview/activity/helper/DividerItemDecoration.java
+++ app/src/main/java/com/example/sawepeter/recylerview/activity/helper/DividerItemDecoration.java
@@ -1,8 +1,13 @@
 package com.example.sawepeter.recylerview.activity.helper;
 
+import android.support.v7.widget.RecyclerView;
+
 /**
  * Created by SAWE PETER on 3/21/2018.
  */
 
-public class DividerItemDecoration {
+public class DividerItemDecoration extends RecyclerView.ItemDecoration{
+    private static final int[] ATTRS = new int[]{
+            android.R.attr.listDivider
+    };
 }
