b6fb13beeecbdd39f4406173b76281ae20234467
user.email <sawepeter6@gmail.com>
2018-03-21 13:13:28 +0300

    variables

--- app/src/main/java/com/example/sawepeter/recylerview/activity/helper/DividerItemDecoration.java
+++ app/src/main/java/com/example/sawepeter/recylerview/activity/helper/DividerItemDecoration.java
@@ -1,5 +1,8 @@
 package com.example.sawepeter.recylerview.activity.helper;
 
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 /**
@@ -10,4 +13,9 @@ public class DividerItemDecoration extends RecyclerView.ItemDecoration{
     private static final int[] ATTRS = new int[]{
             android.R.attr.listDivider
     };
+
+    public static final int HORIZONTAL_LIST = LinearLayoutManager.HORIZONTAL;
+    public static final int VERTICAL_LIST = LinearLayoutManager.VERTICAL;
+    private Drawable mDivider;
+    private int mOrientation;
 }
